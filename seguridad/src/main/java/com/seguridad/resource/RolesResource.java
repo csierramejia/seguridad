6b61a6f83ed00d440f73c8706e93f1ba9d075722
cesar.sierra <cesar.sierra.mejia@gmail.com>
2020-02-09 22:10:41 -0500

    se realiza cambio para retornar objetos dto en lugar de entidad

--- seguridad/src/main/java/com/seguridad/resource/RolesResource.java
+++ seguridad/src/main/java/com/seguridad/resource/RolesResource.java
@@ -1,16 +1,14 @@
 package com.seguridad.resource;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.seguridad.entity.Roles;
+import com.seguridad.dto.RolDTO;
 import com.seguridad.service.RolesService;
 
 import io.swagger.annotations.ApiOperation;
@@ -28,16 +26,16 @@ public class RolesResource {
 	@ApiOperation(value = "Buscar Roles", notes = "Operacion para consultar roles")
 	@ApiResponses(value = { @ApiResponse(code = 201, message = "Roles encontrados"),
 			@ApiResponse(code = 404, message = "Roles no encontrados") })	
-	public ResponseEntity<List<Roles>> findAll() {
-		return ResponseEntity.ok(rolService.findAll());
+	public List<RolDTO> findAll() {
+		return rolService.findAll();
 	}
 
 	@GetMapping("/roles/{id}")
 	@ApiOperation(value = "Buscar Rol", notes = "Operacion para consultar rol por Id")
 	@ApiResponses(value = { @ApiResponse(code = 201, message = "Rol encontrado"),
 			@ApiResponse(code = 404, message = "Rol no encontrado") })	
-	public ResponseEntity<Optional<Roles>> findById(@PathVariable Long id) {
-		return ResponseEntity.ok(rolService.findById(id));
+	public RolDTO findById(@PathVariable Long id) {
+		return rolService.findById(id);
 	} 
     
 }
--- seguridad/src/main/java/com/seguridad/service/RolesService.java
+++ seguridad/src/main/java/com/seguridad/service/RolesService.java
@@ -1,12 +1,15 @@
 package com.seguridad.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.seguridad.dto.RolDTO;
 import com.seguridad.entity.Roles;
 import com.seguridad.repository.RolesRepository;
 
@@ -21,12 +24,33 @@ public class RolesService {
 		this.rolRepository = rolRepository;
 	}
 
-	public Optional<Roles> findById(Long id) {
-		return this.rolRepository.findById(id);
+	public RolDTO findById(Long id) {
+		Optional<Roles> roles = this.rolRepository.findById(id);
+		RolDTO rolDTO = null;
+		
+		if(roles.isPresent()) {
+			rolDTO = new RolDTO();
+		    BeanUtils.copyProperties(roles.get(), rolDTO);
+		}
+		
+		return rolDTO;
 	}
 
-	public List<Roles> findAll() {
-		return this.rolRepository.findAll();
+	public List<RolDTO> findAll() {
+		List<Roles> lstRoles = this.rolRepository.findAll();
+		List<RolDTO> lstRolesDTO = null;
+		
+		if(!lstRoles.isEmpty()) {
+			lstRolesDTO = new ArrayList<RolDTO>();
+			
+		    for (Roles rol: lstRoles) {
+		    	RolDTO rolDTO = new RolDTO();
+		        BeanUtils.copyProperties(rol , rolDTO);
+		        lstRolesDTO.add((RolDTO) rolDTO);
+		     }		    
+		}
+		
+		return lstRolesDTO;
 	}
 	
 	@Transactional
