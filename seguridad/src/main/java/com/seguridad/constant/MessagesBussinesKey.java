6ca54134d9bead2326df27f5d14be183780ee425
Your Name <you@example.com>
2020-02-10 19:06:22 -0500

    Se implementa el enum donde contiene los keys para los mensajes business

--- seguridad/src/main/java/com/seguridad/constant/MessagesBussinesKey.java
+++ seguridad/src/main/java/com/seguridad/constant/MessagesBussinesKey.java
@@ -1,14 +1,12 @@
 package com.seguridad.constant;
 
-import com.seguridad.util.Numero;
-
 /**
  * Se debe mandar el codigo del business messages al cliente (angular, movil)
  */
 public enum MessagesBussinesKey {
 
 	/** 400 - El Usuario y la Contraseña que ingresó no ha sido reconocido.*/
-	KEY_AUTENTICACION_FALLIDA(Numero.UNO.valueI.toString());
+	KEY_AUTENTICACION_FALLIDA("0001");
 
 	public final String value;
 	private MessagesBussinesKey(String value) {
